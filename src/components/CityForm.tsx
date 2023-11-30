1b764cb2bf1bb2e04fb5955e60ce842bcc597758
mlboczar <mlboczar@gmail.com>
2023-11-30 11:10:50 -0500

    Component Hello World!

---
+++ src/components/CityForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CityForm = () => {
+    return (
+        <div>City Form!</div>
+    )
+}
+
+export default CityForm
---
+++ src/components/Weather.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Weather = () => {
+    return (
+        <div>WEATHER!</div>
+    )
+}
+
+export default Weather
